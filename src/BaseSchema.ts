62b2a2dbddf92c718e96b433fe1aaacbf2ed592a
@undefined_is_not_a_function <denismakste@gmail.com>
2025-08-24 11:52:41 +0500

    проблема с совместимостью и пермишонами в миграциях

--- src/BaseSchema.ts
+++ src/BaseSchema.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 import SchemaBuilder from './SchemaBuilder';
 
-export default abstract class BaseMigration {
+export default class BaseMigration {
   protected pool: Pool;
   protected schema: SchemaBuilder;
 
@@ -10,8 +10,12 @@ export default abstract class BaseMigration {
     this.schema = new SchemaBuilder(this.pool);
   }
 
-  abstract up(): Promise<void>;
-  abstract down(): Promise<void>;
+  public up() {
+    throw new Error('Method up is not implemented');
+  }
+  public down() {
+    throw new Error('Method down is not implemented');
+  }
 
   async close(): Promise<void> {
     await this.pool.end();
